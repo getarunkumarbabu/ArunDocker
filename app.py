c4b4b8e536169dcf8c78e8b0c3ccacfd2451e0a1
getarunkumarbabu <getarunkumarbabu@hotmail.com>
2016-08-29 12:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Welcome to Docker with Jenkins CI/CD.. You have been seen %s times.' % redis.get('hits')
+    return 'Welcome to New Docker with Jenkins CI/CD.. You have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
