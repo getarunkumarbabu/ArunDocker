6c75c0881c845523bc88cb845a7e5d6bcd26d2d3
getarunkumarbabu <getarunkumarbabu@hotmail.com>
2016-08-29 14:42:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Welcome to Docker test application with Jenkins CI/CD. This Application is built on Phyton and Redis cache. Visitor Count :%s .' % redis.get('hits')
+    return 'Welcome to New Docker test application with Jenkins CI/CD. This Application is built on Phyton and Redis cache. Visitor Count :%s .' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
